4b76b50242b4b19579e3abc6d23999d88aad2c4c
Boutaina <boutaina.elatbaoui@gmail.com>
2022-09-13 15:13:15 +0100

    Added challenges

--- boucle/challenge-8/challenge8.c
+++ boucle/challenge-8/challenge8.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*Ecrire un programme C qui utilise le principe de dichotomie pour trouver la solution
 de l'équation x3+12x2+1=0 dans l'intervalle [-15,-10] avec une précision de 0,00001.*/
 
 int main()
 {
-    float a,b,m,fa,fb,fm;
-    a=-15;
+    float a, b, m, fa, fb, fm;
+    a = -15;
     b=-10;
-    while(b-a>0.000001)
+    while(b-a>0.00001)
     {
-       m=a+((b-a)/2);
-       fa=a*a*a+12*a*a+1;
-       fb=b*b*b+12*b*b+1;
-       fm=m*m*m+12*m*m+1;
-       if(fa*fm<=0)
-           b=m;
+        m = (a+b)/2;
+       fa = pow(a, 3)+12*(pow(a, 2))+1;
+       fb = pow(b, 3)+12*(pow(b, 2))+1;
+       fm = pow(m, 3)+12*(pow(a, 2))+1;
+       if(fa*fm <= 0)
+           b = m;
        else
-           a=m;
+           a = m;
     }
-    printf("la solution de l'equation x3+12x2+1=0 dans l'intervalle [-15,-10] est :%.5f\n",a);
+    printf("la solution de l'equation x3+12x2+1=0 dans l'intervalle [-15,-10] est :%.4f\n",m);
     return 0;
 }
